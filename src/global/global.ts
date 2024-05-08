653aa8ed9786ac8b9bbd453dd71d47e979878a34
Luka <luka3063671881@outlook.com>
2024-05-08 13:43:36 +0800

    added description.

--- src/global/global.ts
+++ src/global/global.ts
@@ -1,9 +1,14 @@
+//后端地址
 export const baseUrl = "http://***.***.***.***:****";
-export const PORT = "****";
+//websocket服务器端口
+export const PORT = "3322";
+//websocket服务器http地址
 export const HTTP_BASE_URL = "http://***.***.***.***:".concat(PORT);
+//角色模型地址（为什么是websocket服务器的地址，因为懒）
 export const MODEL_BASE_URL = HTTP_BASE_URL.concat("/static/model/");
+//websocket服务器ws地址
 export const WS_BASE_URL = "ws://***.***.***.***:".concat(PORT);
-
+//为什么上面的变量名这么奇怪（改起来有点麻烦，现在能用就好）
 export const fileTypeList = {
     directory: ["directory"],
     video: ["mp4", "avi", "mkv", "wmv", "mpg", "mpeg", "flv"],
